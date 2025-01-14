0ddb787bb4a2bd6c4918bdfc1f59a17feff1ea16
Bilyana Indzheva <bilyana2002@gmail.com>
2025-01-14 22:34:37 +0200

    fix logout

--- pages/auth/logout.php
+++ pages/auth/logout.php
@@ -1,10 +1,28 @@
 <?php
 session_start();
 
-// Remove all session data
+// Remove all session variables
 session_unset();
+
 // Destroy the session
 session_destroy();
+
+// Clear session cookies
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(
+        session_name(),
+        '',
+        time() - 42000,
+        $params["path"],
+        $params["domain"],
+        $params["secure"],
+        $params["httponly"]
+    );
+}
+
+// Optional: Clear other application-specific cookies
+setcookie("your_cookie_name", "", time() - 3600, "/");
 
 // Redirect to login or homepage
 header("Location: login.php");
